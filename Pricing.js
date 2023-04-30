fd4857843997d70b3c89960f18d108c015f3404b
elbaqa <beqa488@gmail.com>
2023-05-01 01:12:02 +0400

    pricing mobile with js

--- Pricing.js
+++ Pricing.js
@@ -12,9 +12,9 @@ function slide(){
         
         slider.style.backgroundColor = '#ffffff';
         slider.style.transform = 'translateX(3.2rem)';
-        slider.style.transition= '0.7s';
+        slider.style.transition= '0.5s';
         box.style.backgroundColor = '#000000';
-        box.style.transition = '1.5s';
+        box.style.transition = '1s';
         pricebasic.innerHTML = (pricebasic.innerHTML * 10).toFixed(2);
         pricepro.innerHTML = (pricepro.innerHTML * 10).toFixed(2);
         pricebus.innerHTML = (pricebus.innerHTML * 10).toFixed(2);
@@ -26,9 +26,9 @@ function slide(){
         
         slider.style.backgroundColor = 'black';
         slider.style.transform = 'translateX(0)';
-        slider.style.transition= '0.7s';
+        slider.style.transition= '0.5s';
         box.style.backgroundColor = '#DFDFDF';
-        box.style.transition = '1.5s';
+        box.style.transition = '1s';
         pricebasic.innerHTML = (pricebasic.innerHTML / 10).toFixed(2);
         pricepro.innerHTML = (pricepro.innerHTML / 10).toFixed(2);
         pricebus.innerHTML = (pricebus.innerHTML / 10).toFixed(2);
@@ -40,3 +40,81 @@ function slide(){
 }
 
 slider.addEventListener('click', slide);
+
+const basic = document.getElementsByClassName("price-card");
+const vis = document.getElementsByClassName("visual");
+
+ 
+function changecolor(a){
+    
+    if(a == 0){
+        basic[a].style.backgroundColor = "black"; 
+        basic[1].style.backgroundColor = "rgba(245, 245, 245, 1)";
+        basic[2].style.backgroundColor = "rgba(245, 245, 245, 1)";    
+        vis[a].style.visibility = "visible"
+        vis[1].style.visibility = "hidden";
+        vis[2].style.visibility = "hidden";
+        basic[a].querySelectorAll("h5")[0].style.color = "white";
+        basic[a].querySelectorAll("p")[0].style.color = "white";
+        basic[a].querySelectorAll("h4")[0].style.color = "white";
+        basic[a].querySelectorAll("button")[0].style.backgroundColor = "white";
+        basic[a].querySelectorAll("button")[0].style.color = "black";
+        basic[1].querySelectorAll("h5")[0].style.color = "black";
+        basic[1].querySelectorAll("p")[0].style.color = "black";
+        basic[1].querySelectorAll("h4")[0].style.color = "black";
+        basic[1].querySelectorAll("button")[0].style.backgroundColor = "black";
+        basic[1].querySelectorAll("button")[0].style.color = "white";
+        basic[2].querySelectorAll("h5")[0].style.color = "black";
+        basic[2].querySelectorAll("p")[0].style.color = "black";
+        basic[2].querySelectorAll("h4")[0].style.color = "black";
+        basic[2].querySelectorAll("button")[0].style.backgroundColor = "black";
+        basic[2].querySelectorAll("button")[0].style.color = "white";
+
+    } else if (a == 1){
+        basic[a].style.backgroundColor = "black"; 
+        basic[0].style.backgroundColor = "rgba(245, 245, 245, 1)";
+        basic[2].style.backgroundColor = "rgba(245, 245, 245, 1)";    
+        vis[a].style.visibility = "visible";
+        vis[0].style.visibility = "hidden";
+        vis[2].style.visibility = "hidden";
+        basic[a].querySelectorAll("h5")[0].style.color = "white";
+        basic[a].querySelectorAll("p")[0].style.color = "white";
+        basic[a].querySelectorAll("h4")[0].style.color = "white";
+        basic[a].querySelectorAll("button")[0].style.backgroundColor = "white";
+        basic[a].querySelectorAll("button")[0].style.color = "black";
+        basic[0].querySelectorAll("h5")[0].style.color = "black";
+        basic[0].querySelectorAll("p")[0].style.color = "black";
+        basic[0].querySelectorAll("h4")[0].style.color = "black";
+        basic[0].querySelectorAll("button")[0].style.backgroundColor = "black";
+        basic[0].querySelectorAll("button")[0].style.color = "white";
+        basic[2].querySelectorAll("h5")[0].style.color = "black";
+        basic[2].querySelectorAll("p")[0].style.color = "black";
+        basic[2].querySelectorAll("h4")[0].style.color = "black";
+        basic[2].querySelectorAll("button")[0].style.backgroundColor = "black";
+        basic[2].querySelectorAll("button")[0].style.color = "white";
+    } else if (a == 2){
+        basic[a].style.backgroundColor = "black"; 
+        basic[0].style.backgroundColor = "rgba(245, 245, 245, 1)";
+        basic[1].style.backgroundColor = "rgba(245, 245, 245, 1)";    
+        vis[a].style.visibility = "visible";
+        vis[0].style.visibility = "hidden";
+        vis[1].style.visibility = "hidden";
+        basic[a].querySelectorAll("h5")[0].style.color = "white";
+        basic[a].querySelectorAll("p")[0].style.color = "white";
+        basic[a].querySelectorAll("h4")[0].style.color = "white";
+        basic[a].querySelectorAll("button")[0].style.backgroundColor = "white";
+        basic[a].querySelectorAll("button")[0].style.color = "black";
+        basic[0].querySelectorAll("h5")[0].style.color = "black";
+        basic[0].querySelectorAll("p")[0].style.color = "black";
+        basic[0].querySelectorAll("h4")[0].style.color = "black";
+        basic[0].querySelectorAll("button")[0].style.backgroundColor = "black";
+        basic[0].querySelectorAll("button")[0].style.color = "white";
+        basic[1].querySelectorAll("h5")[0].style.color = "black";
+        basic[1].querySelectorAll("p")[0].style.color = "black";
+        basic[1].querySelectorAll("h4")[0].style.color = "black";
+        basic[1].querySelectorAll("button")[0].style.backgroundColor = "black";
+        basic[1].querySelectorAll("button")[0].style.color = "white";
+    }
+    
+   
+}
